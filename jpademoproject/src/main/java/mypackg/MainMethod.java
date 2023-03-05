57515cc764ce812ce421eb9911c4f54bfc8a26ed
Govardhan <govardhanduddela522@gmail.com>
2023-03-05 22:26:54 +0530

    commit

---
+++ jpademoproject/src/main/java/mypackg/MainMethod.java
@@ -0,0 +1,12 @@
+package mypackg;
+
+import javax.persistence.Persistence;
+
+public class MainMethod {
+	public static void main(String[] args) {
+		Persistence.createEntityManagerFactory("mypu");
+		
+		
+	}
+
+}
